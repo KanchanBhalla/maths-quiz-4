123a334b67ca1a7cd1d2d6b633bb47cd0f787851
Kanchan Bhalla <50230249+KanchanBhalla@users.noreply.github.com>
2022-06-04 15:51:39 +0530

    Create game_page.js

---
+++ game_page.js
@@ -0,0 +1,21 @@
+document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+	}
+	else 
+	{
+		question_turn = "player1"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+	}
+
+	if(answer_turn == "player1")
+	{
+		answer_turn = "player2"
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+	}
+	else 
+	{
+		answer_turn = "player1"
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+	}
+
+    document.getElementById("output").innerHTML = "";
+}
